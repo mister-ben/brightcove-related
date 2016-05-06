75f8f0b2bb60118f8e4de47002c675b1727eff25
mister-ben <git@misterben.me>
2016-05-06 16:57:54 +0100

    Add polyfill for String.prototype.startsWith()

--- src/secure-url.js
+++ src/secure-url.js
@@ -1,3 +1,10 @@
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+  };
+}
+
 /**
  * Returns the HTTPS equivalent for an HTTP Brightcove CDN URL
  * @constructor
